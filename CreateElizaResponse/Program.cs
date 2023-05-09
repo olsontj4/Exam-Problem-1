f94b2b260f13c9239a33707daf485411f0d9fb49
olsontj4 <olsontj4@my.lanecc.edu>
2023-05-09 14:32:22 -0700

    Exam problem 1 complete.

---
+++ CreateElizaResponse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CreateElizaResponse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string check = "";
+            Console.WriteLine("What seems to be the problem?");
+            while (check != "exit")
+            {
+                check = Console.ReadLine();
+                if (check != "exit")
+                {
+                    Console.WriteLine(CreateElizaResponse(check));
+                }
+            }
+        }
+        static string CreateElizaResponse(string prompt)
+        {
+            string[] genericResponses = { "Tell me more.", "Go on.", "I see.", "Of course." };
+            string[] parse = prompt.Split(' ');
+            for (int i = 0; i < parse.Length; i++)
+            {
+                if (parse[i] == "my")
+                {
+                    return "Tell me more about your " + parse[i + 1];
+                }
+                else if (parse[i] == "love" || parse[i] == "hate")
+                {
+                    return "You feel really strongly about that.";
+                }
+            }
+            return genericResponses[parse.Length % genericResponses.Length];//Feels random, but it's based on the length of the user's input.
+        }
+    }
+}
